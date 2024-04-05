0ced48deb479485a8441405ee037af9e657f8c8c
luisresende13 <luisresende13@gmail.com>
2024-04-05 15:06:46 -0300

    added set up tutorial in set-up.sh script

---
+++ set-up.sh
@@ -0,0 +1,18 @@
+# Install Expo CLI: Open your terminal (Command Prompt, PowerShell, or Terminal) and run the following command to install Expo CLI globally:
+
+npm install -g expo-cli
+
+# Create a new project: Use Expo CLI to create a new React Native project. Navigate to the directory where you want to create the project and run:
+
+expo init YourProjectName
+
+# Navigate to project directory: Move into your newly created project directory:
+
+cd YourProjectName
+
+# Start the development server: Run the following command to start the development server for wither web or android:
+
+npm start
+
+# Install Expo Go app on your Android device: Install the Expo Go app from the Google Play Store on your Android device.
+# Open the Expo Go app on your Android device and scan the QR code displayed in the terminal or in the Expo Dev Tools that opened in your browser when you ran npm start.
